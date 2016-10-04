827cf6ccf034ab6814a2875164fb0851a3b543ea
Aditya Putra Indrayanto <klikadsene@gmail.com>
2016-10-04 13:22:58 +0700

    Add soal ke-4-update

--- exe06.py
+++ exe06.py
@@ -1,2 +1,18 @@
 #Created by : Aditya Putra
 
+data_nim = list()
+data_matkul = list()
+
+jml_mhs = input("Input Jumlah Mahasiswa : ")
+
+for x in range(int(jml_mhs)):
+    nim_mhs = input("Input Nim Mahasiswa Ke-%d : " % (x+1))
+    data_nim.append(str(nim_mhs))
+    jml_matkul = input("Input Jumlah Matkul yang diambil Mahasiswa Ke-%d: " % (x+1))
+    for j in range(int(jml_matkul)):
+        matkul_mhs = raw_input("Matkul Ke-%d : " % (j+1))
+        data_matkul.append(str(matkul_mhs))
+
+
+print data_nim
+print data_matkul
